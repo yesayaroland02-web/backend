cb6177fb3be7479a9c49e5cec708f1c97f7b1e43
yesayaroland02-web <yesayaroland02@gmail.com>
2026-05-14 16:12:16 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,8 @@ app.use('/api/todos', todoRoutes)
 
 app.use('/api/categories', categoryRoutes)
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000')
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
