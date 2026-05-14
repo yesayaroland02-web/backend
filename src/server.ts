932c32f9862ebec15b4f9d4e91c68bcb318b6602
yesayaroland02-web <yesayaroland02@gmail.com>
2026-05-14 17:57:07 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,18 @@ import categoryRoutes from './routes/categoryRoutes'
 
 const app = express()
 
+// CORS (AMAN UNTUK VERCEL)
 app.use(cors({
   origin: "*"
 }))
 
 app.use(express.json())
 
+// ROUTES
 app.use('/api/todos', todoRoutes)
 app.use('/api/categories', categoryRoutes)
 
+// PORT RAILWAY WAJIB ENV
 const PORT: number = Number(process.env.PORT) || 5000
 
 app.listen(PORT, "0.0.0.0", () => {
