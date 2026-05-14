c7e9f55ed24b44b63b15972bbb1ade05c8299989
yesayaroland02-web <yesayaroland02@gmail.com>
2026-05-14 16:30:39 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -10,11 +10,10 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/todos', todoRoutes)
-
 app.use('/api/categories', categoryRoutes)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
