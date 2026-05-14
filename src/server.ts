6630b28b64df7f650c34531d6e612919c30b3a4d
yesayaroland02-web <yesayaroland02@gmail.com>
2026-05-14 17:24:31 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,18 @@ import categoryRoutes from './routes/categoryRoutes'
 
 const app = express()
 
-app.use(cors())
+// 👇 INI DI SINI (WAJIB)
+app.use(cors({
+  origin: "https://frontend-nine-wheat-37.vercel.app"
+}))
+
 app.use(express.json())
 
 app.use('/api/todos', todoRoutes)
 app.use('/api/categories', categoryRoutes)
 
-const PORT: number = Number(process.env.PORT) || 5000;
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
